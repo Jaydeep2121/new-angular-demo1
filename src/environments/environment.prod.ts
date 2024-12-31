6be01d4c8bad2b115fbfecf4f63ac2a7d24a418d
jaydip <jaydipc95@gmail.com>
2024-12-31 07:03:32 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  appVersion: 'v1.1.1',
+  userDataKey: 'loginAuth',
+  accessType:'accessType',
+  apiUrl: 'http://202.47.118.159:86',
+  applicationURL: "https://shriconnect.net/pms/",
+  apiURL: "https://shriconnect.net/pmsapi/",
+  linkURL: "https://clone.shriconnect.net/scgen/",
+  redirectURL: "https://clone.shriconnect.net",
+  baseHref: "/",
+  uiVersion: "0.0.1"
+};
